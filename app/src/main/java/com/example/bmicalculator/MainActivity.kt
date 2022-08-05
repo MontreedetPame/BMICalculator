24ba894cea7f917d3a95a26aa3aae669854a1a40
MontreedetPame <zoozoro@hotmail.com>
2022-08-05 16:48:17 +0700

    สร้างฟังก์ชั่นคำนวณดัชนีมวลกาย โดยรับค่าเมื่อมีการเปลี่ยนแปลงNumberPicker ของนำ้หนัก หรือ ส่วนสูง และแสดงผลบน textView

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.Toast
 import com.example.bmicalculator.databinding.ActivityMainBinding
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,5 +18,34 @@ class MainActivity : AppCompatActivity() {
         binding.weightPicker.maxValue = 150
         binding.heightPicker.minValue = 100
         binding.heightPicker.maxValue = 250
+        binding.weightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+
+        binding.heightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI() {
+        val weight = binding.weightPicker.value;
+        val height = binding.heightPicker.value;
+        val doubleHeight = height.toDouble() / 100
+        val bmi = weight.toDouble() / (doubleHeight * doubleHeight)
+        var textHealthy:String
+        if(bmi < 18.5){
+            textHealthy = "UnderWeight"
+        }else if(bmi < 25.0){
+            textHealthy = "Healthy"
+        }else if(bmi < 30.0){
+            textHealthy = "OverWeight"
+        }else{
+            textHealthy = "Cannot show result"
+        }
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.UP
+        val bmiShow = df.format(bmi)
+        binding.healthyTV.text = "Considered = "+textHealthy
+        binding.resultsTV.text = "Your BMI = "+bmiShow
     }
 }
